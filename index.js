f458416a20612a97ae759b0100729f23a5996a68
HsiTingChou <hero70312@gmail.com>
2018-08-30 01:13:11 +0800

    new

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function handleEvent(event) {
   }
 
   // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
+  const echo = { type: 'text', text: `${event.message.text}${new Date()}`  };
 
   // use reply API
   return client.replyMessage(event.replyToken, echo);
