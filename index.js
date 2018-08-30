6b7e1978f4e4ccf6f87f6f75673998868f585560
HsiTingChou <hero70312@gmail.com>
2018-08-30 09:42:00 +0800

    add range

--- index.js
+++ index.js
@@ -6,12 +6,9 @@ const cheerio = require('cheerio')
 var request = require('request');
 
 
+var today_range;
 var today_verse;
 
-console.log(new Date().toLocaleDateString('ar-zh'));
-console.log(new Date().toLocaleDateString('zh'));
-// console.log(new Date().toLocaleDateString('zh'));
-//
 // function doEveryDay() {
 //     request('http://www.duranno.tw/livinglife/index.php/daily', function (error, response, body) {
 //         console.log('error:', error); // Print the error if one occurred
@@ -32,6 +29,9 @@ request('http://www.duranno.tw/livinglife/index.php/daily', function (error, res
     // console.log('error:', error); // Print the error if one occurred
     // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     // console.log('body:', body); // Print the HTML for the Google homepage.
+    const $ = cheerio.load(body);
+    // console.log();
+
     var str = body;
     var n = str.indexOf("MyJSStringVar");
     var d = str.indexOf("var div = document.getElementById('c_cont');");
@@ -40,7 +40,10 @@ request('http://www.duranno.tw/livinglife/index.php/daily', function (error, res
     var verse = str.substring( n + 16, d - 5);
     // console.log(verse);
     today_verse = verse.replace(/([";.*+^$[\]\\(){}-])/g,'');
+    today_range = $('.range').text().replace(/ /g,'');
     // console.log(today_verse);
+    let a = `${today_range}\n${today_verse}\n${new Date().toLocaleDateString('zh')}`
+    console.log(a);
 });
 
 
@@ -85,7 +88,7 @@ function handleEvent(event) {
 
   // create a echoing text message
   // const echo = { type: 'text', text: `${event.message.text}${new Date()}`  };
-  const echo = { type: 'text', text: `${today_verse}  ${new Date().toLocaleDateString('zh')}`};
+  const echo = { type: 'text', text: `${today_range}\n${today_verse}\n${new Date().toLocaleDateString('zh')}`};
 
   console.log('client', client);
 
